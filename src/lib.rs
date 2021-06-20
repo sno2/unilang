151d74c70cd9351cef358481d04fabaa97a5955f
Carter Snook <cartersnook04@gmail.com>
2021-06-20 12:04:22 -0500

    test: add test for basic function to rust/typescript

--- src/lib.rs
+++ src/lib.rs
@@ -7,32 +7,8 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn it_works() {
-		let func = Function {
-			name: Box::new(String::from("subtract")),
-			visibility: Visibility::Public,
-			return_type: Some(Box::new(String::from("i32"))),
-			params: vec![
-				Parameter {
-					name: Box::new("a"),
-					typ: Some(Box::new("i32")),
-				},
-				Parameter {
-					name: Box::new("b"),
-					typ: Some(Box::new("i32")),
-				},
-			],
-			content: vec![Box::new(statement::VariableInit {
-				name: Box::new(String::from("difference")),
-				mutable: Some(false),
-				typ: None,
-				value: Box::new(operation::Add("a", "b")),
-			})],
-		};
-
-		let language = Language::TypeScript;
-
-		let func2 = Function::default()
+	fn basic_function() {
+		let func = Function::default()
 			.with_visibility(Visibility::Public)
 			.with_name("add")
 			.with_return_type(types::Number)
@@ -45,11 +21,14 @@ mod tests {
 			)
 			.with_statement(statement::Return(Some("sum")));
 
-		let extension = match language {
-			Language::Rust => "rs",
-			Language::TypeScript => "ts",
-		};
+		assert_eq!(
+			func.to_code(Language::Rust),
+			"pub fn add(a:i32,b:i32)->i32{let sum=a+b;return sum;}"
+		);
 
-		std::fs::write(format!("./test.{}", extension), func2.to_code(language)).unwrap();
+		assert_eq!(
+			func.to_code(Language::TypeScript),
+			"export function add(a:number,b:number):number{const sum=a+b;return sum;}"
+		);
 	}
 }
