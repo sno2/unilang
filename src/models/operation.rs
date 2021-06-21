57e337dd8d8f8aec6651fce97d8519cf7974909b
Carter Snook <cartersnook04@gmail.com>
2021-06-20 23:51:00 -0500

    chore(operation): use macro for creating operations

--- src/models/operation.rs
+++ src/models/operation.rs
@@ -1,59 +1,26 @@
 use crate::{Language, ToCode};
 
-// TODO: create macro for this
+macro_rules! new_operation {
+	($name:ident, $operator:expr) => {
+		#[derive(Debug)]
+		pub struct $name<T: ToCode, F: ToCode>(pub T, pub F);
 
-#[derive(Debug)]
-pub struct Add<T: ToCode, F: ToCode>(pub T, pub F);
-
-impl<T: ToCode, F: ToCode> ToCode for Add<T, F> {
-	fn to_code(&self, language: Language) -> String {
-		let Self(a, b) = self;
-		match language {
-			Language::Rust | Language::TypeScript => {
-				format!("{}+{}", a.to_code(language), b.to_code(language))
+		impl<T: ToCode + std::fmt::Display, F: ToCode + std::fmt::Display> ToCode for $name<T, F> {
+			fn to_code(&self, language: Language) -> String {
+				let Self(a, b) = self;
+				format!(
+					"{}{}{}",
+					a.to_code(language),
+					$operator,
+					b.to_code(language)
+				)
 			}
 		}
-	}
+	};
 }
 
-#[derive(Debug)]
-pub struct Subtract<T: ToCode, F: ToCode>(pub T, pub F);
-
-impl<T: ToCode, F: ToCode> ToCode for Subtract<T, F> {
-	fn to_code(&self, language: Language) -> String {
-		let Self(a, b) = self;
-		match language {
-			Language::Rust | Language::TypeScript => {
-				format!("{}-{}", a.to_code(language), b.to_code(language))
-			}
-		}
-	}
-}
-
-#[derive(Debug)]
-pub struct Multiply<T: ToCode, F: ToCode>(pub T, pub F);
-
-impl<T: ToCode, F: ToCode> ToCode for Multiply<T, F> {
-	fn to_code(&self, language: Language) -> String {
-		let Self(a, b) = self;
-		match language {
-			Language::Rust | Language::TypeScript => {
-				format!("{}*{}", a.to_code(language), b.to_code(language))
-			}
-		}
-	}
-}
-
-#[derive(Debug)]
-pub struct Divide<T: ToCode, F: ToCode>(pub T, pub F);
-
-impl<T: ToCode, F: ToCode> ToCode for Divide<T, F> {
-	fn to_code(&self, language: Language) -> String {
-		let Self(a, b) = self;
-		match language {
-			Language::Rust | Language::TypeScript => {
-				format!("{}*{}", a.to_code(language), b.to_code(language))
-			}
-		}
-	}
-}
+new_operation!(Add, "+");
+new_operation!(Subtract, "-");
+new_operation!(Multiply, "*");
+new_operation!(Divide, "/");
+new_operation!(Modulus, "%");
