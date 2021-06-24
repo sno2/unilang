4bdfbeb468c61eacf23bd51b73b0aee3afb1b1e8
Carter Snook <cartersnook04@gmail.com>
2021-06-23 22:48:46 -0500

    test: fix broken tests

--- src/lib.rs
+++ src/lib.rs
@@ -11,31 +11,40 @@ mod tests {
 
 	#[test]
 	fn basic_function() {
-		let scope = Scope::default()
-			.with(Import::Members(
-				String::from("foo"),
-				vec![String::from("Hello")],
-			))
-			.with(
-				Function::default()
-					.with_visibility(Visibility::Public)
-					.with_name("add")
-					.with_return_type(types::Number)
-					.with_param(Parameter(Box::new("a"), Some(Box::new(types::Number))))
-					.with_param(Parameter(Box::new("b"), Some(Box::new(types::Number))))
-					.with_scope(
-						Scope::default().with(statement::Return(Some(operation::Add("a", "b")))),
-					),
-			);
+		let scope = Scope::default().with(
+			Function::default()
+				.with_visibility(Visibility::Public)
+				.with_name("add")
+				.with_return_type(types::Integer)
+				.with_param(Parameter(Box::new("a"), Some(Box::new(types::Integer))))
+				.with_param(Parameter(Box::new("b"), Some(Box::new(types::Integer))))
+				.with_scope(
+					Scope::default().with(statement::Return(Some(operation::Add("a", "b")))),
+				),
+		);
 
 		assert_eq!(
 			scope.to_code(Language::Rust),
-			"pub fn add(a:i32,b:i32)->i32{return a+b;}"
+			"pub fn add(a:i32,b:i32)->i32{return (a+b);}"
 		);
 
 		assert_eq!(
 			scope.to_code(Language::TypeScript),
-			"export function add(a:number,b:number):number{return a+b;}"
+			"export function add(a:number,b:number):number{return (a+b);}"
+		);
+
+		assert_eq!(
+			scope.to_code(Language::CPP),
+			"int add(int a,int b){return (a+b);}"
+		);
+
+		assert_eq!(
+			scope.to_code(Language::Python {
+				include_types: true,
+				indent_level: None,
+				indent_type: IndentType::Tab
+			}),
+			"\ndef add(a:int,b:int)->int:\n\treturn (a+b)"
 		);
 	}
 }
